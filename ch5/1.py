e97ade72351b60bcce0a47451bfba7e50b3be409
AMDyesIntelno <2621023869@qq.com>
2020-09-03 11:25:50 +0800

    update

---
+++ ch5/1.py
@@ -0,0 +1,17 @@
+#删除一个list中的重复元素
+s=[1,1,2,3,4,2,6,7,6]
+print(list(set(s)))
+#---
+temp=[]
+for i in range(len(s)):
+    if s.index(s[i])<i:
+        continue
+    else:
+        temp.append(s[i])
+print(temp)
+#---
+temp=[]
+for i in s:
+    if i not in temp:
+        temp.append(i)
+print(temp)
---
+++ ch5/2.py
@@ -0,0 +1,7 @@
+#求列表中的元素个数,最大最小值,元素之和,平均值
+s=[9,7,8,3,2,1,55,6]
+print(len(s))
+print(max(s))
+print(min(s))
+print(sum(s))
+print(sum(s)/len(s))
