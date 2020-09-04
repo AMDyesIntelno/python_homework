6dd3dd3e43da4f83634789dea329614d5cfc254b
AMDyesIntelno <2621023869@qq.com>
2020-09-04 11:44:19 +0800

    update

---
+++ ch6/5.1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys,random
+a=int(sys.argv[1])
+for i in range(a):
+    print(random.randint(0,100))
---
+++ ch6/5.2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+start=int(sys.argv[1])
+end=int(sys.argv[2])
+for line in sys.stdin:
+    value=int(line)
+    if start<=value<=end:
+        print(value)
+# ./ch6/5.1.py 60 | ./ch6/5.2.py 20 60
