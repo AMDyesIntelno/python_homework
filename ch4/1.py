839e4edd338fae5167652023e7243864eb90bee6
AMDyesIntelno <2621023869@qq.com>
2020-10-09 17:22:06 +0800

    update

--- ch4/1.py
+++ ch4/1.py
@@ -1,6 +1,11 @@
-#格式化输出杨辉三角
-print("1".center(20))
-print("1 1".center(20))
-print("1 2 1".center(20))
-print("1 3 3 1".center(20))
-print("1 4 6 4 1".center(20))
+a=[0,1,0]
+for i in range(5):
+    s=""
+    for j in range(1,len(a)-1):
+        s+=str(a[j])+" "
+    print(s.center(20))
+    b=[0]
+    for j in range(len(a)-1):
+        b.append(a[j]+a[j+1])
+    b.append(0)
+    a=b
